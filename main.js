200e58e30246bd7392072590bc1e818e800346ad
Askralos <moeun@pc-fixe-sm>
2023-03-23 16:41:27 +0100

    feat : css

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
-function createNode(element) {
-  return document.createElement(element);
-}
-
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-
-const ul = document.getElementById("authors");
-const url = "https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json";
-
-fetch(url)
+fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
   .then((resp) => resp.json())
-  .then(function (data) {
-    let tables = data.results;
-    return tables.map(function (table) {
-      let tr = createNode("tr");
-      let td= createNode("td");
-      let img = createNode("img");
-      td.innerHTML = `${author} ${author.name.last}`;
-      append(tr, img);
-      append(tr, td);
-      append(tbody, tr);
-    });
+  .then((apiData) => {
+    
   })
   .catch(function (error) {
     console.log(error);
