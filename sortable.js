9ed5d7763dcdafa91e6493643635892a96a1ebcb
Askralos <moeun@pc-fixe-sm>
2023-03-23 16:43:59 +0100

    rename fichier

--- sortable.js
+++ sortable.js
@@ -1,8 +1,11 @@
-fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
+function api(){
+  fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
   .then((resp) => resp.json())
   .then((apiData) => {
-    
+
   })
   .catch(function (error) {
     console.log(error);
   });
+
+}
