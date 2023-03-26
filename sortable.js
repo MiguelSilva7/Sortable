bf056cb4f24386998be96553ab7fb6ebeb7f9f46
Askralos <moeun@pc-fixe-sm>
2023-03-26 21:12:56 +0200

    feat: trie dossier

--- sortable.js
+++ sortable.js
@@ -1,137 +1,79 @@
-// This function is called only after the data has been fetched, and parsed.
-function loadData2(heroes) {
-  const heroArrays = Object.values(heroes);
+document.addEventListener("DOMContentLoaded", init, false);
 
-  const heroimage = heroArrays.map((hero) => hero.images.xs);
-  const heroname = heroArrays.map((hero) => hero.name);
-  const herofullname = heroArrays.map((hero) => hero.biography.fullName);
-  const heropowers = heroArrays.map((hero) => hero.powerstats);
-  const herorace = heroArrays.map((hero) => hero.appearance.race);
-  const herogender = heroArrays.map((hero) => hero.appearance.gender);
-  const heroheight = heroArrays.map((hero) => hero.appearance.height);
-  const heroweight = heroArrays.map((hero) => hero.appearance.weight);
-  const herobirth = heroArrays.map((hero) => hero.biography.placeOfBirth);
-  const heroalignement = heroArrays.map((hero) => hero.biography.alignment);
+let data, table, sortCol;
+let sortAsc = false;
+let pageSize = 20;
+let curPage = 1;
 
-  let hero = [];
-  let temp = [];
+async function init() {
+  // Select the table (well, tbody)
+  table = document.querySelector("#table-sortable tbody");
+  // get the cats
+  let resp = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json");
+  data = await resp.json();
+  renderTable();
 
-  for (let i = 0; i < heroname.length; i++) {
-    temp.push(heroimage[i]); //0
-    temp.push(heroname[i]); // 1
-    temp.push(herofullname[i]); //2
-    temp.push(heropowers[i]);
-    temp.push(herorace[i]);
-    temp.push(herogender[i]);
-    temp.push(heroheight[i]);
-    temp.push(heroweight[i]);
-    temp.push(herobirth[i]);
-    temp.push(heroalignement[i]);
-    hero.push(temp);
-
-    temp = [];
-  }
-  return hero;
-}
-
-// Request the file with fetch, the data will downloaded to your browser cache.
-fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
-  .then((response) => response.json()) // parse the response from JSON
-  .then((loadData) => {
-    hero = loadData2(loadData);
-
-    // console.log(hero);
-
-    function newRow(hero) {
-      let table = document.getElementById("table-sortable");
-
-      for (let i = 0; i < hero.length; i++) {
-        const row = table.insertRow();
-
-        const iconCell = row.insertCell();
-        let icon = document.createElement("img");
-        icon.src = hero[i][0];
-        iconCell.appendChild(icon);
-
-        const nameCell = row.insertCell();
-        nameCell.textContent = hero.sortByName(hero)[i][1];
-
-        const fullNameCell = row.insertCell();
-        fullNameCell.textContent = hero[i][2];
-
-        const powerstatCell = row.insertCell();
-        powerstatCell.textContent = hero[i][3];
-
-        const raceCell = row.insertCell();
-        raceCell.textContent = hero[i][4];
-
-        const genderCell = row.insertCell();
-        genderCell.textContent = hero[i][5];
-
-        const heightCell = row.insertCell();
-        heightCell.textContent = hero[i][6][1];
-
-        const weightCell = row.insertCell();
-        weightCell.textContent = hero[i][7][1];
-
-        const birthCell = row.insertCell();
-        birthCell.textContent = hero[i][8];
-
-        const alignementCell = row.insertCell();
-        alignementCell.textContent = hero[i][9];
-      }
-    }
-    newRow(hero);
+  // listen for sort clicks
+  document.querySelectorAll("#table-sortable thead tr th").forEach((t) => {
+    t.addEventListener("click", sort, false);
   });
 
-function sortByName(hero) {
-  heroname.sort((a, b) => a[1].localeCompare(b[1]));
-}
-
-function sortByFullName(hero) {
-  herofullname.sort((a, b) => a[2].localeCompare(b[2]));
-}
-
-function sortByRace(hero) {
-  herorace.sort((a, b) => a[4].localeCompare(b[4]));
-}
-
-function sortByGender(hero) {
-  herogender.sort((a, b) => a[5].localeCompare(b[5]));
-}
-
-function sortByHeight(hero) {
-  heroheight.sort((a, b) => {
-    const aHeight = parseInt(a[6][1]);
-    const bHeight = parseInt(b[6][1]);
-    return aHeight - bHeight;
+  // listen for page size select changes
+  document.querySelector("#page-size-select").addEventListener("change", (event) => {
+    pageSize = parseInt(event.target.value);
+    curPage = 1;
+    renderTable();
   });
 }
 
-function sortByWeight(hero) {
-  heroweight.sort((a, b) => {
-    const aWeight = parseInt(a[7][1]);
-    const bWeight = parseInt(b[7][1]);
-    return aWeight - bWeight;
+document.querySelector("#nextButton").addEventListener("click", nextPage, false);
+document.querySelector("#prevButton").addEventListener("click", previousPage, false);
+
+function renderTable() {
+  // create html
+  let result = "";
+  data
+    .filter((row, index) => {
+      let start = (curPage - 1) * pageSize;
+      let end = curPage * pageSize;
+      if (index >= start && index < end) return true;
+    })
+    .forEach((c) => {
+      result += `<tr>
+<td><img src="${c.images.xs}" alt=""></td>
+<td>${c.name}</td>
+<td>${c.biography.fullName}</td>
+<td>${c.powerstats}</td>
+<td>${c.appearance.race}</td>
+<td>${c.appearance.gender}</td>
+<td>${c.appearance.height[1]}</td>
+<td>${c.appearance.weight[1]}</td>
+<td>${c.biography.placeOfBirth}</td>
+<td>${c.biography.alignment}</td>
+</tr>`;
+    });
+  table.innerHTML = result;
+}
+
+function sort(e) {
+  let thisSort = e.target.dataset.sort;
+  if (sortCol === thisSort) sortAsc = !sortAsc;
+  sortCol = thisSort;
+  console.log("sort dir is ", sortAsc);
+  data.sort((a, b) => {
+    if (a[sortCol] < b[sortCol]) return sortAsc ? 1 : -1;
+    if (a[sortCol] > b[sortCol]) return sortAsc ? -1 : 1;
+    return 0;
   });
+  renderTable();
 }
 
-function sortByBirth(hero) {
-  herobirth.sort((a, b) => a[8].localeCompare(b[8]));
+function previousPage() {
+  if (curPage > 1) curPage--;
+  renderTable();
 }
 
-function sortByAlignement(array) {
-  let goodArray = [];
-  let badArray = [];
-  let sortArray = goodArray.concat(badArray);
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] == "good") {
-      goodArray.push(array[i]);
-    } else {
-      badArray.push(array[i]);
-    }
-  }
-  return sortArray;
+function nextPage() {
+  if (curPage * pageSize < data.length) curPage++;
+  renderTable();
 }
-
-console.log(sortByAlignement(['good','bad','good','bad','good','good','bad','good','bad','bad','bad','bad','good','good',]));
