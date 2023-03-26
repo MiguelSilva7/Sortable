0d2a59054fbff2f7a3eb475d5d29701b85afbeff
Zafake <brandaoangelo2004@gmail.com>
2023-03-26 12:29:30 +0200

    page-size

--- sortable.js
+++ sortable.js
@@ -1,3 +1,39 @@
+function loadData2(heroes) {
+    const heroArrays = Object.values(heroes);
+  
+    const heroimage = heroArrays.map((hero) => hero.images.xs);
+    const heroname = heroArrays.map((hero) => hero.name);
+    const herofullname = heroArrays.map((hero) => hero.biography.fullName);
+    const heropowers = heroArrays.map((hero) => hero.powerstats);
+    const herorace = heroArrays.map((hero) => hero.appearance.race);
+    const herogender = heroArrays.map((hero) => hero.appearance.gender);
+    const heroheight = heroArrays.map((hero) => hero.appearance.height);
+    const heroweight = heroArrays.map((hero) => hero.appearance.weight);
+    const herobirth = heroArrays.map((hero) => hero.biography.placeOfBirth);
+    const heroalignement = heroArrays.map((hero) => hero.biography.alignment);
+  
+    let hero = [];
+    let temp = [];
+  
+    for (let i = 0; i < heroname.length; i++) {
+      temp.push(heroimage[i]); //0
+      temp.push(heroname[i]); // 1
+      temp.push(herofullname[i]); //2
+      temp.push(heropowers[i]);
+      temp.push(herorace[i]);
+      temp.push(herogender[i]);
+      temp.push(heroheight[i]);
+      temp.push(heroweight[i]);
+      temp.push(herobirth[i]);
+      temp.push(heroalignement[i]);
+      hero.push(temp);
+  
+      temp = [];
+    }
+    // console.log(hero[1][1]);
+    return hero;
+  }
+
 const loadData = heroes => {
     console.log(heroes)
   }
@@ -24,4 +60,46 @@ fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
                     result.append(tr)
                 })
             })
+    }
+
+    function newRow(hero) {
+        let table = document.getElementById("table-sortable");
+        
+        for (let i = 0; i < hero.length; i++) {
+          
+          const row = table.insertRow();
+          
+          const iconCell = row.insertCell();
+          let icon = document.createElement('img')
+          icon.src = hero[i][0];
+          iconCell.appendChild(icon);
+          
+          const nameCell = row.insertCell();
+          nameCell.textContent = hero[i][1];
+          
+          const fullNameCell = row.insertCell();
+          fullNameCell.textContent = hero[i][2];
+          
+          const powerstatCell = row.insertCell();
+          powerstatCell.textContent = hero[i][3];
+          
+          const raceCell = row.insertCell();
+          raceCell.textContent = hero[i][4];
+          
+          const genderCell = row.insertCell();
+          genderCell.textContent = hero[i][5];
+          
+          const heightCell = row.insertCell();
+          heightCell.textContent = hero[i][6][1];
+          
+          const weightCell = row.insertCell();
+          weightCell.textContent = hero[i][7][1];
+          
+          const birthCell = row.insertCell();
+          birthCell.textContent = hero[i][8];
+          
+          const alignementCell = row.insertCell();
+          alignementCell.textContent = hero[i][9];
+        }
+        newRow(hero);
     }
