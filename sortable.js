aaedf66626cd8336639f1296ef51806aeb6b2aa2
Zafake <brandaoangelo2004@gmail.com>
2023-03-26 16:50:50 +0200

    filter

--- sortable.js
+++ sortable.js
@@ -1,3 +1,4 @@
+
 function loadData2(heroes) {
     const heroArrays = Object.values(heroes);
   
@@ -14,7 +15,7 @@ function loadData2(heroes) {
   
     let hero = [];
     let temp = [];
-  
+    
     for (let i = 0; i < heroname.length; i++) {
       temp.push(heroimage[i]); //0
       temp.push(heroname[i]); // 1
@@ -39,15 +40,62 @@ const loadData = heroes => {
   }
 fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
     .then((response) => response.json()) // parse the response from JSON
-   .then(loadData) // .then will call the `loadData` function with the JSON value.
- 
-    const myInput = document.querySelector("#myInput")
+    // .then will call the `loadData` function with the JSON value.
+    .then((loadData) => {
+    hero = loadData2(loadData);
+
+    console.log(hero);
+
+    function newRow(hero) {
+      let table = document.getElementById("table-sortable");
+      
+      for (let i = 0; i < hero.length; i++) {
+        
+        const row = table.insertRow();
+        
+        const iconCell = row.insertCell();
+        let icon = document.createElement('img')
+        icon.src = hero[i][0];
+        iconCell.appendChild(icon);
+        
+        const nameCell = row.insertCell();
+        nameCell.textContent = hero[i][1];
+        
+        const fullNameCell = row.insertCell();
+        fullNameCell.textContent = hero[i][2];
+        
+        const powerstatCell = row.insertCell();
+        powerstatCell.textContent = hero[i][3];
+        
+        const raceCell = row.insertCell();
+        raceCell.textContent = hero[i][4];
+        
+        const genderCell = row.insertCell();
+        genderCell.textContent = hero[i][5];
+        
+        const heightCell = row.insertCell();
+        heightCell.textContent = hero[i][6][1];
+        
+        const weightCell = row.insertCell();
+        weightCell.textContent = hero[i][7][1];
+        
+        const birthCell = row.insertCell();
+        birthCell.textContent = hero[i][8];
+        
+        const alignementCell = row.insertCell();
+        alignementCell.textContent = hero[i][9];
+      }
+    }
+    newRow(hero); 
+});
+
+const myInput = document.querySelector("#myInput")
+
     btn.onclick = (e) => {
         e.preventDefault()
         fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
         .then((response) => response.json()) // parse the response from JSON
         .then (loadData => {
-            const result = document.querySelector("#result")
             const input = document.querySelector("#nomhero").value
             const newFilter = loadData.filter(instant => instant.name.toLowerCase().includes(input.toLowerCase()))
             result.textContent = "";
