1391704192bccbecc3840de9d5b603209465906f
Zafake <brandaoangelo2004@gmail.com>
2023-03-26 21:21:06 +0200

    search

--- sortable.js
+++ sortable.js
@@ -1,98 +1,100 @@
 
-function loadData2(heroes) {
-    const heroArrays = Object.values(heroes);
+// function loadData2(heroes) {
+//     const heroArrays = Object.values(heroes);
   
-    const heroimage = heroArrays.map((hero) => hero.images.xs);
-    const heroname = heroArrays.map((hero) => hero.name);
-    const herofullname = heroArrays.map((hero) => hero.biography.fullName);
-    const heropowers = heroArrays.map((hero) => hero.powerstats.intelligence);
-    const herorace = heroArrays.map((hero) => hero.appearance.race);
-    const herogender = heroArrays.map((hero) => hero.appearance.gender);
-    const heroheight = heroArrays.map((hero) => hero.appearance.height);
-    const heroweight = heroArrays.map((hero) => hero.appearance.weight);
-    const herobirth = heroArrays.map((hero) => hero.biography.placeOfBirth);
-    const heroalignement = heroArrays.map((hero) => hero.biography.alignment);
+//     const heroimage = heroArrays.map((hero) => hero.images.xs);
+//     const heroname = heroArrays.map((hero) => hero.name);
+//     const herofullname = heroArrays.map((hero) => hero.biography.fullName);
+//     const heropowers = heroArrays.map((hero) => hero.powerstats.intelligence);
+//     const herorace = heroArrays.map((hero) => hero.appearance.race);
+//     const herogender = heroArrays.map((hero) => hero.appearance.gender);
+//     const heroheight = heroArrays.map((hero) => hero.appearance.height);
+//     const heroweight = heroArrays.map((hero) => hero.appearance.weight);
+//     const herobirth = heroArrays.map((hero) => hero.biography.placeOfBirth);
+//     const heroalignement = heroArrays.map((hero) => hero.biography.alignment);
   
-    let hero = [];
-    let temp = [];
+//     let hero = [];
+//     let temp = [];
     
-    for (let i = 0; i < heroname.length; i++) {
-      temp.push(heroimage[i]); //0
-      temp.push(heroname[i]); // 1
-      temp.push(herofullname[i]); //2
-      temp.push(heropowers[i]);
-      temp.push(herorace[i]);
-      temp.push(herogender[i]);
-      temp.push(heroheight[i]);
-      temp.push(heroweight[i]);
-      temp.push(herobirth[i]);
-      temp.push(heroalignement[i]);
-      hero.push(temp);
+//     for (let i = 0; i < heroname.length; i++) {
+//       temp.push(heroimage[i]); //0
+//       temp.push(heroname[i]); // 1
+//       temp.push(herofullname[i]); //2
+//       temp.push(heropowers[i]);
+//       temp.push(herorace[i]);
+//       temp.push(herogender[i]);
+//       temp.push(heroheight[i]);
+//       temp.push(heroweight[i]);
+//       temp.push(herobirth[i]);
+//       temp.push(heroalignement[i]);
+//       hero.push(temp);
   
-      temp = [];
-    }
-    // console.log(hero[1][1]);
-    return hero;
-  }
+//       temp = [];
+//     }
+//     // console.log(hero[1][1]);
+//     return hero;
+//   }
 
-const loadData = heroes => {
-    console.log(heroes)
-  }
-fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
-    .then((response) => response.json()) // parse the response from JSON
-    // .then will call the `loadData` function with the JSON value.
-    .then((loadData) => {
-    hero = loadData2(loadData);
+// const loadData = heroes => {
+//     console.log(heroes)
+//   }
+// fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
+//     .then((response) => response.json()) // parse the response from JSON
+//     // .then will call the `loadData` function with the JSON value.
+//     .then((loadData) => {
+//     hero = loadData2(loadData);
 
-    console.log(hero);
+//     console.log(hero);
 
-    function newRow(hero) {
-      let table = document.getElementById("table-sortable");
+//     function newRow(hero) {
+//       let table = document.getElementById("table-sortable");
       
-      for (let i = 0; i < hero.length; i++) {
+//       for (let i = 0; i < hero.length; i++) {
         
-        const row = table.insertRow();
+//         const row = table.insertRow();
         
-        const iconCell = row.insertCell();
-        let icon = document.createElement('img')
-        icon.src = hero[i][0];
-        iconCell.appendChild(icon);
+//         const iconCell = row.insertCell();
+//         let icon = document.createElement('img')
+//         icon.src = hero[i][0];
+//         iconCell.appendChild(icon);
         
-        const nameCell = row.insertCell();
-        nameCell.textContent = hero[i][1];
+//         const nameCell = row.insertCell();
+//         nameCell.textContent = hero[i][1];
         
-        const fullNameCell = row.insertCell();
-        fullNameCell.textContent = hero[i][2];
+//         const fullNameCell = row.insertCell();
+//         fullNameCell.textContent = hero[i][2];
         
-        const powerstatCell = row.insertCell();
-        powerstatCell.textContent = hero[i][3];
+//         const powerstatCell = row.insertCell();
+//         powerstatCell.textContent = hero[i][3];
         
-        const raceCell = row.insertCell();
-        raceCell.textContent = hero[i][4];
+//         const raceCell = row.insertCell();
+//         raceCell.textContent = hero[i][4];
         
-        const genderCell = row.insertCell();
-        genderCell.textContent = hero[i][5];
+//         const genderCell = row.insertCell();
+//         genderCell.textContent = hero[i][5];
         
-        const heightCell = row.insertCell();
-        heightCell.textContent = hero[i][6][1];
+//         const heightCell = row.insertCell();
+//         heightCell.textContent = hero[i][6][1];
         
-        const weightCell = row.insertCell();
-        weightCell.textContent = hero[i][7][1];
+//         const weightCell = row.insertCell();
+//         weightCell.textContent = hero[i][7][1];
         
-        const birthCell = row.insertCell();
-        birthCell.textContent = hero[i][8];
+//         const birthCell = row.insertCell();
+//         birthCell.textContent = hero[i][8];
         
-        const alignementCell = row.insertCell();
-        alignementCell.textContent = hero[i][9];
-      }
-    }
-    newRow(hero); 
-});
+//         const alignementCell = row.insertCell();
+//         alignementCell.textContent = hero[i][9];
+//       }
+//     }
+//     newRow(hero); 
+// });
 
 document.addEventListener("DOMContentLoaded", init, false);
 
 let data, table, sortCol;
 let sortAsc = false;
+let pageSize = 10;
+let curPage = 1;
 
 async function init() {
   // Select the table (well, tbody)
@@ -106,13 +108,29 @@ async function init() {
   document.querySelectorAll("#table-sortable thead tr th").forEach((t) => {
     t.addEventListener("click", sort, false);
   });
+
+  // listen for page size select changes
+  document.querySelector("#page-size-select").addEventListener("change", (event) => {
+    pageSize = parseInt(event.target.value);
+    curPage = 1;
+    renderTable();
+  });
 }
+
+document.querySelector("#nextButton").addEventListener("click", nextPage, false);
+document.querySelector("#prevButton").addEventListener("click", previousPage, false);
 
 function renderTable() {
   // create html
   let result = "";
-  data.forEach((c) => {
-    result += `<tr>
+  data
+    .filter((row, index) => {
+      let start = (curPage - 1) * pageSize;
+      let end = curPage * pageSize;
+      if (index >= start && index < end) return true;
+    })
+    .forEach((c) => {
+      result += `<tr>
 <td><img src="${c.images.xs}" alt=""></td>
 <td>${c.name}</td>
 <td>${c.biography.fullName}</td>
@@ -124,7 +142,7 @@ function renderTable() {
 <td>${c.biography.placeOfBirth}</td>
 <td>${c.biography.alignment}</td>
 </tr>`;
-  });
+    });
   table.innerHTML = result;
 }
 
@@ -132,11 +150,22 @@ function sort(e) {
   let thisSort = e.target.dataset.sort;
   if (sortCol === thisSort) sortAsc = !sortAsc;
   sortCol = thisSort;
+  console.log("sort dir is ", sortAsc);
   data.sort((a, b) => {
     if (a[sortCol] < b[sortCol]) return sortAsc ? 1 : -1;
     if (a[sortCol] > b[sortCol]) return sortAsc ? -1 : 1;
     return 0;
   });
+  renderTable();
+}
+
+function previousPage() {
+  if (curPage > 1) curPage--;
+  renderTable();
+}
+
+function nextPage() {
+  if (curPage * pageSize < data.length) curPage++;
   renderTable();
 }
 
@@ -162,3 +191,25 @@ const myInput = document.querySelector("#myInput")
                 })
             })
     }
+
+// 		function myFunction() {
+//     // Declare variables
+//     let input, filter, table, tr, td, i, txtValue;
+//     input = document.getElementById("myInput");
+//     filter = input.value.toUpperCase();
+//     table = document.getElementById("table-sortable");
+//     tr = table.getElementsByTagName("tr");
+
+// // Loop through all table rows, and hide those who don't match the search query
+//     for (i = 0; i < tr.length; i++) {
+//       td = tr[i].getElementById("td")[1];
+//       if (td) {
+//         txtValue = td.textContent || td.innerText
+//             if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//           tr[i].style.display = "";
+//             }else {
+//           tr[i].style.display = "none";
+//             }
+//          }
+//     }
+// }
